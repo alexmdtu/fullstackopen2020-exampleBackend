3e1b5dd6a2f72ee1269b80b0c915224238392e81
Alexander Tu <alexander.md.tu@gmail.com>
2020-03-30 15:31:21 +0200

    added frontend build to backend

---
+++ build/precache-manifest.bd4497d52d3a13a17eb71c68b2615edf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e48f53f28e580360cba14e7bc533552",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c3bc744c391347f410e",
+    "url": "/static/css/main.f73f7164.chunk.css"
+  },
+  {
+    "revision": "7ff3afc94969683bc841",
+    "url": "/static/js/2.eb8923a7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.eb8923a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c3bc744c391347f410e",
+    "url": "/static/js/main.4785c440.chunk.js"
+  },
+  {
+    "revision": "7e65d857e43f4173d1e8",
+    "url": "/static/js/runtime-main.7813ffa7.js"
+  }
+]);
---
+++ build/precache-manifest.e78f0d597b144bd450de5f2b86b1bb0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569d70035257da87b99d5bb826eeb27c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c35bb696da9326e8ca0",
+    "url": "/static/css/main.f73f7164.chunk.css"
+  },
+  {
+    "revision": "7ff3afc94969683bc841",
+    "url": "/static/js/2.eb8923a7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.eb8923a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c35bb696da9326e8ca0",
+    "url": "/static/js/main.435a4c69.chunk.js"
+  },
+  {
+    "revision": "7e65d857e43f4173d1e8",
+    "url": "/static/js/runtime-main.7813ffa7.js"
+  }
+]);
